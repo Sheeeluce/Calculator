af439a16d98b04e062770781f3701c3e7f1e6ac1
Lucy <lucymwago9@gmail.com>
2026-03-26 08:22:40 +0300

    fixing js and undefined functions

--- script.js
+++ script.js
@@ -5,7 +5,34 @@ function appendToDisplay(value){
   display.value += value;
 }
 
+
+let memory = null
+
 function clearDisplay(){
+  memory = Number(display.value);
   display.value= "";
 }
 
+function calculate(){
+
+  let operator;
+  if (display.value.includes('+')) operator = '+'
+  else if (display.value.includes('-')) operator = '-'
+  else if (display.value.includes('*')) operator = '*'
+  else if (display.value.includes('/')) operator = '/'
+
+
+  let parts = display.value.split(operator);
+
+  let x = Number(parts[0]);
+  let y = Number(parts[1]);
+
+  let result;
+
+  if (operator === "+") result = (x+y);
+  else if (operator === "-") result = (x-y);
+  else if (operator === "*") result = (x*y);
+  else if (operator === "/") result = (x/y);
+
+  display.value = result;
+}
